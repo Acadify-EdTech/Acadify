a5e13fe179085943685a5671eca7cf288a5e94ec
harshitshukla-dev <hershitshukla35@gmail.com>
2024-02-16 12:11:51 +0530

    Refactor CSS and JavaScript code for responsive design

--- public/Assets/js/navbar.js
+++ public/Assets/js/navbar.js
@@ -1,5 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var toggleButton = document.querySelector('.toggleBtn');
+    toggleButton.addEventListener('click', toggleNav);
+});
+
 function toggleNav() {
     var menuDiv = document.getElementById("MenuDiv");
-    menuDiv.style.width = menuDiv.style.width === "250px" ? "0" : "250px";
+    menuDiv.style.width = menuDiv.style.width === "100%" ? "0" : "100%";
 }
-
