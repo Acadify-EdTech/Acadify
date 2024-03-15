e5201f14f87a98918d315aaf257f07f703dc11ec
harshitshukla-dev <hershitshukla35@gmail.com>
2024-03-16 00:07:32 +0530

    Add Ace code editor and implement code running functionality

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
+const { spawn } = require('child_process');
 
 const app = express();
 const port = process.env.PORT || 3000;
 
 // Serve static files from the 'public' directory
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // Define routes for each HTML file
 app.get('/', (req, res) => {
@@ -28,6 +32,28 @@ app.get('/quizportal',(req,res)=>{
   res.sendFile(path.join(__dirname, 'public', 'pages', 'quizportal.html'));
 });
 
+app.post('/run', function(req, res) {
+  const fs = require('fs');
+  fs.writeFileSync('temp.cpp', req.body.code);
+
+  const compile = spawn('g++', ['temp.cpp']);
+  compile.on('close', function (code) {
+    if (code !== 0) {
+      return res.send('Compilation error');
+    }
+
+    const run = spawn('./a.out');
+    let output = '';
+
+    run.stdout.on('data', function (data) {
+      output += data.toString();
+    });
+
+    run.on('close', function (code) {
+      res.send(output);
+    });
+  });
+});
 // Add more routes for other HTML files as needed
 
 // Start the server
