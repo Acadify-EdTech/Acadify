6d55ea9ae37ac45da5e161a5330c504b4cb710fb
harshitshukla-dev <hershitshukla35@gmail.com>
2024-03-23 15:08:22 +0530

    Add dashboard route and component

--- Acadify-Frontend/src/app/app.component.ts
+++ Acadify-Frontend/src/app/app.component.ts
@@ -1,34 +1,17 @@
 import { Component, inject, Signal, CUSTOM_ELEMENTS_SCHEMA, ElementRef, ViewChild, Renderer2} from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterLink } from '@angular/router';
 import '@material/web/all';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, RouterLink, DashboardComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
   schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-//import { Component, ElementRef, ViewChild } from '@angular/core';
-
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   templateUrl: './app.component.html',
-//   styleUrl: './app.component.css',
-//   schemas: [CUSTOM_ELEMENTS_SCHEMA]
-// })
 
 export class AppComponent {
-  @ViewChild('sidebar') sidebar!: ElementRef;
-
-  constructor(private renderer: Renderer2) {}
-
-  toggleSidebar() {
-    this.sidebar.nativeElement.classList.contains('close') ?
-      this.renderer.removeClass(this.sidebar.nativeElement, 'close') :
-      this.renderer.addClass(this.sidebar.nativeElement, 'close');
-  }
 }
--- Acadify-Frontend/src/app/app.routes.ts
+++ Acadify-Frontend/src/app/app.routes.ts
@@ -1,3 +1,7 @@
 import { Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: 'dashboard', pathMatch: 'full' },
+    { path: 'dashboard', component: DashboardComponent }
+];
