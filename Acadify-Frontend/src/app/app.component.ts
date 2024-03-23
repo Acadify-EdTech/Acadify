a82ce55e201c100ab2ccd12bf88f6b8eb4e07d69
harshitshukla-dev <hershitshukla35@gmail.com>
2024-03-23 15:02:04 +0530

    Fix HTML and TypeScript formatting and update styles.css

--- Acadify-Frontend/src/app/app.component.ts
+++ Acadify-Frontend/src/app/app.component.ts
@@ -1,35 +1,34 @@
-// import { Component, inject, Signal, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-// import '@material/web/all';
+import { Component, inject, Signal, CUSTOM_ELEMENTS_SCHEMA, ElementRef, ViewChild, Renderer2} from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import '@material/web/all';
 
-
-// @Component({
-//   selector: 'app-root',
-//   standalone: true,
-//   imports: [RouterOutlet],
-//   templateUrl: './app.component.html',
-//   styleUrl: './app.component.css',
-//   schemas: [CUSTOM_ELEMENTS_SCHEMA]
-// })
-import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   standalone: true,
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppComponent {
-  @ViewChild('sidebar', { static: true }) sidebar!: ElementRef;
-  
-  toggleSidebar() {
-    this.sidebar.nativeElement.classList.toggle('close');
-  }
+//import { Component, ElementRef, ViewChild } from '@angular/core';
 
-  toggleSubmenu(event: Event) {
-    let parent = (event.target as Element).parentElement?.parentElement;
-    if (parent) {
-      parent.classList.toggle('showMenu');
-    }
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   templateUrl: './app.component.html',
+//   styleUrl: './app.component.css',
+//   schemas: [CUSTOM_ELEMENTS_SCHEMA]
+// })
+
+export class AppComponent {
+  @ViewChild('sidebar') sidebar!: ElementRef;
+
+  constructor(private renderer: Renderer2) {}
+
+  toggleSidebar() {
+    this.sidebar.nativeElement.classList.contains('close') ?
+      this.renderer.removeClass(this.sidebar.nativeElement, 'close') :
+      this.renderer.addClass(this.sidebar.nativeElement, 'close');
   }
 }
