82b1131c8cbdd4133028628066dda1c6ee6ae6fe
harshitshukla-dev <hershitshukla35@gmail.com>
2024-04-02 11:21:49 +0530

    Add connections to multiple MongoDB databases

--- Acadify-Backend/apiserver.js
+++ Acadify-Backend/apiserver.js
@@ -2,22 +2,43 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
-
 const app = express();
 const port = process.env.PORT || 4000;
 
 app.use(cors());
 app.use(bodyParser.json());
 
-mongoose.connect('mongodb://localhost:27017/quizdb', { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => console.log('MongoDB connected...'))
-    .catch(err => console.log(err));
+const conn1 = mongoose.createConnection('mongodb://localhost:27017/quizdb');
+const conn2 = mongoose.createConnection('mongodb://localhost:27017/CodeDB');
 
+conn1.on('open', () => {
+    console.log('Connection 1 to MongoDB established...');
+});
+
+conn1.on('error', (err) => {
+    console.log(err);
+});
+
+conn2.on('open', () => {
+    console.log('Connection 2 to MongoDB established...');
+});
+
+conn2.on('error', (err) => {
+    console.log(err);
+});
 const questionSchema = new mongoose.Schema({}, { strict: false }); // Use a non-strict schema to allow any structure
-const Question = mongoose.model('Question', questionSchema, 'questions'); // The 'questions' collection
+const Question = conn1.model('Question', questionSchema, 'questions'); // The 'questions' collection
 
 app.get('/api/questions', async (req, res) => {
     const questions = await Question.find({});
+    res.json(questions);
+});
+
+const questionSchema1 = new mongoose.Schema({}, { strict: false }); // Use a non-strict schema to allow any structure
+const Question1 = conn2.model('Question1', questionSchema1, 'sample'); // The 'sample' collection
+
+app.get('/api/questions1', async (req, res) => {
+    const questions = await Question1.find({});
     res.json(questions);
 });
 
