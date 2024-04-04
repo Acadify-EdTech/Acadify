b1d6feca00de198819b009150bf970ed7805dea6
harshitshukla-dev <hershitshukla35@gmail.com>
2024-04-04 18:42:33 +0530

    Update API endpoint in codeEditor.js

--- Acadify-Frontend/public/assets/js/codeEditor.js
+++ Acadify-Frontend/public/assets/js/codeEditor.js
@@ -68,7 +68,7 @@ document.addEventListener('DOMContentLoaded', function () {
     let userCode = [];
 
     window.onload = async function () {
-        const response = await fetch("http://localhost:4000/api/questions1");
+        const response = await fetch("https://bh0gs0nl-4000.inc1.devtunnels.ms/api/questions1");
         questions = await response.json();
 
         userCode = new Array(questions.length).fill('');
